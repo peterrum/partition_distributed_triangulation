fce0186671c3b7b6b358d68c7990ff0373ba62b9
Peter Munch <peterrmuench@gmail.com>
2021-05-26 09:31:16 +0200

    Continue

--- main.cc
+++ main.cc
@@ -44,14 +44,81 @@ namespace dealii
     namespace Utilities
     {
       template <int dim, int spacedim>
+      struct DescriptionTemp
+      {
+        template <class Archive>
+        void
+        serialize(Archive &ar, const unsigned int /*version*/)
+        {
+          ar &coarse_cells;
+          ar &coarse_cell_vertices;
+          ar &coarse_cell_index_to_coarse_cell_id;
+          ar &cell_infos;
+        }
+
+        std::vector<dealii::CellData<dim>> coarse_cells;
+
+        std::vector<std::pair<unsigned int, Point<spacedim>>>
+          coarse_cell_vertices;
+
+        std::vector<types::coarse_cell_id> coarse_cell_index_to_coarse_cell_id;
+
+        std::vector<std::vector<CellData<dim>>> cell_infos;
+      };
+
+      template <int dim, int spacedim>
+      void
+      fill_cell_infos(const TriaIterator<CellAccessor<dim, spacedim>> &cell,
+                      std::vector<std::vector<CellData<dim>>> &cell_infos)
+      {
+        cell->set_user_flag();
+
+        CellData<dim> cell_info;
+
+        // save coarse-cell id
+        cell_info.id = cell->id().template to_binary<dim>();
+
+        // save boundary_ids of each face of this cell
+        for (const auto f : cell->face_indices())
+          {
+            types::boundary_id boundary_ind = cell->face(f)->boundary_id();
+            if (boundary_ind != numbers::internal_face_boundary_id)
+              cell_info.boundary_ids.emplace_back(f, boundary_ind);
+          }
+
+        // save manifold id
+        {
+          // ... of cell
+          cell_info.manifold_id = cell->manifold_id();
+
+          // ... of lines
+          if (dim >= 2)
+            for (const auto line : cell->line_indices())
+              cell_info.manifold_line_ids[line] =
+                cell->line(line)->manifold_id();
+
+          // ... of quads
+          if (dim == 3)
+            for (const auto f : cell->face_indices())
+              cell_info.manifold_quad_ids[f] = cell->quad(f)->manifold_id();
+        }
+
+        // subdomain and level subdomain id
+        cell_info.subdomain_id       = cell->subdomain_id();
+        cell_info.level_subdomain_id = cell->level_subdomain_id();
+
+        cell_infos[cell->level()].emplace_back(cell_info);
+
+        if (cell->level() != 0)
+          fill_cell_infos(cell->parent(), cell_infos);
+      }
+
+      template <int dim, int spacedim>
       Description<dim, spacedim>
       create_description_from_triangulation(
         const Triangulation<dim, spacedim> &              tria,
         const LinearAlgebra::distributed::Vector<double> &partition)
       {
-        (void)tria;
-        (void)partition;
-
         const auto relevant_processes = [&]() {
           std::set<unsigned int> relevant_processes;
 
@@ -62,6 +129,13 @@ namespace dealii
                                            relevant_processes.end());
         }();
 
+        // 1) collect locally relevant cells (set user_flag)
+        std::vector<bool> old_user_flags;
+        tria.save_user_flags(old_user_flags);
+
+        // 1a) clear user_flags
+        const_cast<dealii::Triangulation<dim, spacedim> &>(tria)
+          .clear_user_flags();
 
         std::map<unsigned int, std::vector<unsigned int>>
                                              coinciding_vertex_groups;
@@ -70,14 +144,11 @@ namespace dealii
         GridTools::collect_coinciding_vertices(
           tria, coinciding_vertex_groups, vertex_to_coinciding_vertex_group);
 
-        // helper function, which collects all vertices belonging to a cell
-        // (also taking into account periodicity)
         const auto
           add_vertices_of_cell_to_vertices_owned_by_locally_owned_cells =
             [&coinciding_vertex_groups, &vertex_to_coinciding_vertex_group](
               const auto &       cell,
               std::vector<bool> &vertices_owned_by_locally_owned_cells) {
-              // add local vertices
               for (const auto v : cell->vertex_indices())
                 {
                   vertices_owned_by_locally_owned_cells[cell->vertex_index(v)] =
@@ -93,10 +164,17 @@ namespace dealii
                 }
             };
 
-        for (const auto &proc : relevant_processes)
+        std::vector<DescriptionTemp<dim, spacedim>> description_temp(
+          relevant_processes.size());
+
+        for (unsigned int i = 0; i < description_temp.size(); ++i)
           {
+            const unsigned int proc               = relevant_processes[i];
+            auto &             description_temp_i = description_temp[i];
+
             std::cout << proc << std::endl;
 
+            // mark all vertices attached to locally owned cells
             std::vector<bool> vertices_owned_by_locally_owned_cells_on_level(
               tria.n_vertices());
             for (const auto &cell : tria.active_cell_iterators())
@@ -113,14 +191,58 @@ namespace dealii
               return false;
             };
 
+            description_temp_i.cell_infos.resize(
+              tria.get_triangulation().n_global_levels());
+
+            // collect locally relevant cells (including their parents)
             for (const auto &cell : tria.active_cell_iterators())
               if (is_locally_relevant_on_level(cell))
-                std::cout << cell->id() << std::endl;
+                fill_cell_infos(cell, description_temp_i.cell_infos);
+
+            // collect coarse-grid cells
+            std::vector<bool> vertices_locally_relevant(tria.n_vertices(),
+                                                        false);
+
+            for (const auto &cell : tria.cell_iterators_on_level(0))
+              {
+                if (!cell->user_flag_set())
+                  continue;
+
+                // extract cell definition (with old numbering of vertices)
+                dealii::CellData<dim> cell_data(cell->n_vertices());
+                cell_data.material_id = cell->material_id();
+                cell_data.manifold_id = cell->manifold_id();
+                for (const auto v : cell->vertex_indices())
+                  cell_data.vertices[v] = cell->vertex_index(v);
+                description_temp_i.coarse_cells.push_back(cell_data);
+
+                // save indices of each vertex of this cell
+                for (const auto v : cell->vertex_indices())
+                  vertices_locally_relevant[cell->vertex_index(v)] = true;
+
+                // save translation for corase grid: lid -> gid
+                description_temp_i.coarse_cell_index_to_coarse_cell_id
+                  .push_back(cell->id().get_coarse_cell_id());
+              }
+
+            // collect coarse-grid vertices
+            for (unsigned int i = 0; i < vertices_locally_relevant.size(); ++i)
+              if (vertices_locally_relevant[i])
+                description_temp_i.coarse_cell_vertices.emplace_back(
+                  i, tria.get_vertices()[i]);
           }
 
+        const_cast<dealii::Triangulation<dim, spacedim> &>(tria)
+          .load_user_flags(old_user_flags);
 
+        DescriptionTemp<dim, spacedim> description_merged;
+        description_merged.cell_infos.resize(tria.n_global_levels());
+
+        (void)description_temp;
 
         Description<dim, spacedim> description;
+
+        (void)description_merged;
 
         return description;
       }
